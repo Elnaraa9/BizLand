1fbe89f5ad5a006148c1754893ac9591d8263e66
Elnaraa9 <7khtht32code.edu.az>
2022-06-23 12:14:43 +0400

    demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "35px 10px";
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "25px 10px";
+    document.getElementById("logo").style.fontSize = "35px 10px";
   }
 }
